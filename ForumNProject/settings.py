01551aa69161694d71eaef5e2092b939b4b204d9
savaman <savam.0510@gmail.com>
2024-02-28 18:09:45 +0300

    "28.02.2024" Откат

--- ForumNProject/settings.py
+++ ForumNProject/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'media'
+    BASE_DIR / 'media',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
